aefac8d96f373572da9f482c644d8ac4cec86c60
GabrielgsdCIUwU <holagabriel321@gmail.com>
2025-04-02 10:50:01 +0200

    feat(ArnoldMain): Añadir input usuario

--- src/main/java/edu/estatuas/ArnoldMain.java
+++ src/main/java/edu/estatuas/ArnoldMain.java
@@ -1,9 +1,19 @@
 package edu.estatuas;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class ArnoldMain {
     public static void main(String[] args) {
 
-        double peso = 175.0; // kg on earth
+        System.out.println("Welcome to the Arnold Schwarzenegger weight calculator!");
+        System.out.println("You will be able to calculate your weight on different planets in the solar system.");
+
+
+        System.out.println("Please enter your weight on Earth in kg: ");
+        
+        double peso = tryGetPeso();        
+
 
         for (Planeta planeta : Planeta.values()) {
             System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
@@ -19,5 +29,16 @@ public class ArnoldMain {
             System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
         }
         
+    }
+    private static double tryGetPeso() {
+        Scanner in = new Scanner(System.in);
+        try {
+            return in.nextDouble();
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input. Please enter a number.");
+            return tryGetPeso();
+        } finally {
+            in.close();
+        }
     }
 }
